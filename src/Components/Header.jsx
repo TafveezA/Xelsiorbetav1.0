6b469b200852833f00dc01791e8aaf100394363b
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-09-25 11:31:57 +0400

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -203,7 +203,7 @@ function Navbar() {
                                         Earn
                                     </NavLink>
                                 </li>
-                                   </li>
+                                
                                 
 
                             </ul>
