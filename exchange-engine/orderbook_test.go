6e992bea7c192d355939062adeae006e6ae380be
TafveezA <tafveez.alphadev@gmail.com>
2023-09-29 14:57:03 +0400

    added exchange engine in go

---
+++ exchange-engine/orderbook_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func assert(t *testing.T, a, b any) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("%v !=%+v", a, b)
+	}
+}
+
+func TestLimit(t *testing.T) {
+	l := NewLimit(10_000)
+	buyOrderA := NewOrder(true, 5)
+	buyOrderB := NewOrder(true, 5)
+	buyOrderC := NewOrder(true, 5)
+	l.AddOrder(buyOrderA)
+	l.AddOrder(buyOrderB)
+	l.AddOrder(buyOrderC)
+
+	l.DeleteOrder(buyOrderB)
+	fmt.Println(l)
+}
+
+func TestPlaceLimitOrder(t *testing.T) {
+	ob := NewOrderbook()
+	sellOrderA := NewOrder(false, 10)
+	sellOrderB := NewOrder(false, 5)
+	ob.PlaceLimitOrder(10_000, sellOrderA)
+	ob.PlaceLimitOrder(9_000, sellOrderB)
+	assert(t, len(ob.asks), 2)
+}
