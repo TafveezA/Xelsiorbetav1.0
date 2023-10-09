65f81c719f6d2f7805fb93eb37d9da76d388cd9c
TafveezA <123924708+TafveezA@users.noreply.github.com>
2023-10-09 15:16:00 +0400

    Update price.js

--- backend/routes/price.js
+++ backend/routes/price.js
@@ -1,1 +1,9 @@
+const express = require('express')
+const {tokenPrice} = require('../controllers/priceapi')
 
+const router = express.Router()
+
+router.route('/').get(tokenPrice)
+
+
+module.exports = router;
